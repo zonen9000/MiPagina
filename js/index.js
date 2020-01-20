b52df6ffc81985ec8afda60e06cdc57e1dc6a9d8
zonen9000 <zorencode9000@gmail.com>
2020-01-20 12:27:01 -0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    //  var elems = document.querySelectorAll('.sidenav');
+     // var instances = M.Sidenav.init(elems);
+    M.AutoInit();
+   });
+
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
